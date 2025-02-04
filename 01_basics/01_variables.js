69689c58c54efb9278d29d77f96e9964a9cc1c1f
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-04 18:55:29 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 171800
+let accountEmail = "mayanaumar@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState
+
+// accountId = 33 // constant changing is not allowed
+
+accountEmail = "umar@ac.in"
+accountPassword = "00"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, acccountState]);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
