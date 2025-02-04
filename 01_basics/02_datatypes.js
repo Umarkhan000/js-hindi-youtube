d99e1defc7a97cd32aa066723409a806ea9abf13
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-04 18:56:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // this "use strict" will treat all js code as newer code or version
+
+// alert( 3 + 3) /// we are using nodejs compiler to run, not browser
+
+
+let name = "umarkhan"
+let age = 18
+let isLoggedin = false
+let state;
+
+
+// numbers => 2 to power 53
+// if the number is very big then use - bigint
+// string => mostly prefer double quotes ""
+// boolea => true/false
+// null => null is a standalone value
+// undefined => this means not defining anyvalue in the variale
+// symbol => this helps to find any unique thing
+
+
+// object
+//exaple of object below
+console.log(typeof null)  //output shows as "object"
+console.log(typeof undefined)  //output is undefined only because we didn't assign anything
+
+
