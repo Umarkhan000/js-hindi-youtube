9d50ad7cda4f6cb7541961b48b7a801cabbfdf35
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-05 17:59:18 +0000

    conversion of datatypes. Some operation topics

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -17,7 +17,7 @@ console.log(valueInNumber);
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=> true; 0=> false
 // "" => false 
@@ -28,5 +28,39 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber)
+
+
+// ***************** Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log (negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " umar"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study conversions
+// https://262.ecma-international.org/#sec-type-conversion
