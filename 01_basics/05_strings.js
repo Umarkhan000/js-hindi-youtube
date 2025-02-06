26f9c8c52d7d010c609bbf4912160840384eb71e
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-06 19:04:46 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "umar"
+const repoCount = 60
+
+// console.log(name + repoCount);  // don't use this instead use modern way by using backtick(`) and ${}
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+
