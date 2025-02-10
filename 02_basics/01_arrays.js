43a6f761dbd0733014bea20e77ffd736a202794d
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-10 12:05:31 +0000

    arrays in javascript

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,15 +4,34 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeroes = ["kohli", "abd"]
 
 const myArr2 = (1, 2, 3, 4)
-console.log(myArr[0]);
+// console.log(myArr[0]);
+// console.log(myHeroes);
+// console.log(myArr2);
 
 
 // Array methods
 
-myArr.push(6)    // push prints the numbers till you want
-myArr.push(8)
-myArr.pop()     // pop removes the last digit in the given numbers
+// myArr.push(6)    // push prints the numbers till you want
+// myArr.push(8)
+// myArr.pop()     // pop removes the last digit in the given numbers
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(0));
+
+
+const newArr = myArr.join()
 console.log(myArr);
+console.log(newArr);
+
+
+
+
+
+
+
 
 
 
