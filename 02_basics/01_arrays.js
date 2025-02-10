0afc975d22c0084b7740d096292aa1cb8681a5b1
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-10 18:10:10 +0000

    arrays in javascript

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -23,12 +23,27 @@ const myArr2 = (1, 2, 3, 4)
 
 
 const newArr = myArr.join()
-console.log(myArr);
-console.log(newArr);
+// console.log(myArr);
+// console.log(newArr);
 
 
+// slice and splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+
+console.log(myn1);
+console.log("c", myArr);
+console.log(myn2);
 
 
+console.log();
 
 
 
