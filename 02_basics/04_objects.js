b68faa8c5a7c7ac81ee7855bcbb0f75ab2138e46
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-15 19:13:05 +0000

    json talk and objects in JS

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,12 +27,79 @@ const regularUser = {
 
 
 
-const obj1 = {1: "a", 2:"b"}
+const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 
-const obj3 = Object.assign(obj1, obj2)
+//const obj3 = Object.assign({}, obj1, obj2)
 
-console.log(obj3);
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "umar@gmail.com"
+    },
+    {
+        id: 2,
+        email: "khan@gmail.com"
+    },
+]
+
+
+// console.log(Object.keys(instagramUser));
+// console.log(Object.values(instagramUser));
+// console.log(Object.entries(instagramUser));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+courseTeacher: "umarkhan"
+}
+
+const {courseTeacher: teacher} = course  // this is called DESTRUCTURE, if the name is big you can give small name within it.
+console.log(teacher);
+
+
+
+
+
+// JSON is written in curly braces{} and both key and values are both should be written in strings " " 
+// {
+//     "name": "umar",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+   {},
+   {},
+   {},
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
