c0dcdfff37fe6dce590a18cbf4a04416889b8a52
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-16 17:37:43 +0000

    functions in javascript

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+function sayMyName(){
+   console.log("u");
+   console.log("m");
+   console.log("a");
+   console.log("r");
+}
+
+// sayMyName()
+
+
+function addTwonumbers(number1, number2){
+        console.log(number1 + number2);
+}
+
+addTwonumbers(5,9)
+addTwonumbers(109,null)
+
+
+
+
+
+
+
+
