ccc20f734ce75867d480184e26d9b948944c0747
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-17 18:03:24 +0000

    funtions in javascript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,12 +8,46 @@ function sayMyName(){
 // sayMyName()
 
 
-function addTwonumbers(number1, number2){
-        console.log(number1 + number2);
-}
+// function addTwonumbers(number1, number2){
+//   console.log(number1 + number2);
+// }
 
 addTwonumbers(5,9)
-addTwonumbers(109,null)
+
+
+function addTwonumbers(number1, number2){
+ 
+        // let result = number1 + number2
+        // return result
+        return number1 + number2
+ }
+        
+const result = addTwonumbers(46, 85)
+//console.log("Result: ", result);
+
+
+
+function loginUserMessage(username){
+        return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("umarahamedkhan"));
+
+
+
+// using IF statement
+function loginUserMessage(username){
+        if(username === undefined){
+                console.log("please enter a username");
+                return
+        }
+        return `${username} just logged in`
+}
+console.log(loginUserMessage());
+
+
+
+
 
 
 
