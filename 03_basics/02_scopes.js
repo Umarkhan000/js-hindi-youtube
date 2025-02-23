62d95dae429e8879b82c866f746e179621906a0e
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-23 18:07:00 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,45 @@
+
+//var c = 30
+let a = 300
+
+if (true){
+     let a = 10
+     const b = 20
+     var c = 30
+     console.log("IINER : ", a);
+     
+}
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
