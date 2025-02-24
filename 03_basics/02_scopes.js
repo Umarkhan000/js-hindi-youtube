c05bdc6617eeca07d6e90067228f6e159ed3f107
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-24 18:28:13 +0000

    scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,28 +6,71 @@ if (true){
      let a = 10
      const b = 20
      var c = 30
-     console.log("IINER : ", a);
+     //  console.log("IINER : ", a);
      
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 
 
+// NESTED SCOPE
+
+function one(){
+     const username = "umarkhan"
+
+function two(){
+     const website = "youtube"
+     // console.log(username);
+     }
+    // console.log(website);   // this is error cannot be printed
+     
+
+     two()
+}
+
+// one()
+
+
+
+if(true) {
+      const username = "umarkhan"
+      if (username === "umarkhan"){
+          const website = " youtube"
+          // console.log(username + website);
+          
+      }
+     // console.log(website);  // this is error cannot be printed
+     
+     
+}
+     // console.log(username);   // this is error cannot be printed
+     
 
 
 
 
 
+// +++++++++++++++++++++++++ INTERESTING ++++++++++++++++++++++++++
 
 
 
+console.log(addone(5));
+
+function addone(num){
+     return num + 1
+}
+ 
 
 
+console.log(addtwo(5));
+const addtwo = function(num){
+     return num + 2              // this can't be run coz we defined the it by const so it can't. Up one it called it directly through function so it can be run
+}
 
 
 
