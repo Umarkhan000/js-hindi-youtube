3e679a046b6ff7e6e232afef81de51b253d28811
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-25 18:42:34 +0000

    arrow in js

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,33 @@
+const user = {
+    username : "umarkhan",
+    price : 999, 
+ 
+ 
+      welcomeMessage : function(){
+      console.log(`${this.username} , welcome to website`);
+      console.log(this);
+      }
+ 
+ }
+ 
+//  user.welcomeMessage()
+//  user.username = "ibrahim"
+//  user.welcomeMessage()
+ 
+// console.log(this);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
