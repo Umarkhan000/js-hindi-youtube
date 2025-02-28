c948cd4c330ca76226c98afd25eba220f207cef0
Mayana Umarkhan <mayanaumarahamedkhan@gmail.com>
2025-02-28 18:46:36 +0000

    IIFE in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,23 @@
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+
+(function chai (){
+    console.log(`DB CONNECTED`);
+}) ();
+
+// IIFE IN ARROW FUNCTION
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+} ) ("umarkhan")
+
+
+
+
+
+
+
+
+
+
